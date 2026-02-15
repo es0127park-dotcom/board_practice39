46cf443ccfd4e1f5d93b54e1680e4a4b16b1e2cd
parkcoding <es0127.park@gmail.com>
2026-02-15 19:45:01 +0900

    개발 시작

---
+++ src/main/java/com/example/board_practice39/BoardPractice39Application.java
@@ -0,0 +1,13 @@
+package com.example.board_practice39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPractice39Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardPractice39Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/board_practice39/BoardPractice39ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board_practice39;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPractice39ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
